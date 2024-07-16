4b31e2e61fd6add56ac5cbf6af44768b4ac7358f
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-16 20:29:41 +0600

    clear all the task

--- taskmate/src/componets/ShowTask.js
+++ taskmate/src/componets/ShowTask.js
@@ -6,7 +6,7 @@ export const ShowTask = ({taskList, setTaskList}) => {
                 <span className="title">Todo</span>
                 <span className="count">{taskList.length}</span>
             </div>
-            <button className="clearAll">Clear All</button>
+            <button className="clearAll" onClick={()=>setTaskList([0])}>Clear All</button>
         </div>
         <ul>
             {taskList.map((task)=>(
