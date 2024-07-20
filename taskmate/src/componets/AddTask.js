72b55e0edf6ca9a90d9ae553ece55709e7a09e6c
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-20 19:06:28 +0600

    Warning Undefined value

--- taskmate/src/componets/AddTask.js
+++ taskmate/src/componets/AddTask.js
@@ -8,6 +8,7 @@ export const AddTask = ({taskList, setTaskList, task, setTask}) => {
         todo.id === task.id? {id: task.id, name:task.name, time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`}: todo 
       ));
       setTaskList(updatedTasklist);
+      setTask({});
 
     } else{
       const date = new Date();
@@ -17,7 +18,7 @@ export const AddTask = ({taskList, setTaskList, task, setTask}) => {
         time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
       }
       setTaskList([...taskList, newTask]);
-      e.target.task.value = "";
+      setTask({});
   
     }
 
@@ -26,7 +27,7 @@ export const AddTask = ({taskList, setTaskList, task, setTask}) => {
   return (
     <section className="addTask">
         <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task.name} autoComplete="off" placeholder="add task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})}/>
+            <input type="text" name="task" value={task.name|| ""} autoComplete="off" placeholder="add task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})}/>
             <button type="submit">Add</button>
     
         </form>
