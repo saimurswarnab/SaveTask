694edf218cd0d3c1040a4e5707eea77b978025fc
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-07-22 19:12:10 +0600

    Final project

--- taskmate/src/componets/AddTask.js
+++ taskmate/src/componets/AddTask.js
@@ -27,7 +27,7 @@ export const AddTask = ({taskList, setTaskList, task, setTask}) => {
   return (
     <section className="addTask">
         <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task.name|| ""} autoComplete="off" placeholder="add task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})}/>
+            <input type="text" name="task" value={task.name|| ""} autoComplete="off" placeholder="add task" maxLength={50} onChange={e => setTask({...task, name: e.target.value})}/>
             <button type="submit">{task.id?"Update": "Add"}</button>
     
         </form>
